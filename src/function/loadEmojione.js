7dad60b69997afcdf4562c1a6bc3e4057fa0a42d
Osama Bashir <traien93@gmail.com>
2022-02-22 13:51:25 +0300

    check if emojione exist before get it from cdn

--- src/function/loadEmojione.js
+++ src/function/loadEmojione.js
@@ -11,63 +11,95 @@ define([
 ],
 function(emojione, uniRegexp, readyCallbacks, emojioneSupportMode, cdn, getEmojioneVersion, emojioneReady, detectVersion, getSupportMode) {
     return function(options) {
-        var emojioneVersion = getEmojioneVersion()
+        let emojioneVersion = getEmojioneVersion();
+
         options = getOptions(options);
 
-        if (!cdn.isLoading) {
-            if (!emojione || getSupportMode(detectVersion(emojione)) < 2) {
-                cdn.isLoading = true;
-                var emojioneJsCdnUrlBase;
-                if (getSupportMode(emojioneVersion) > 5) {
-                    emojioneJsCdnUrlBase = cdn.defaultBase3 + "npm/emojione@" + emojioneVersion;
-                } else if (getSupportMode(emojioneVersion) > 4) {
-                    emojioneJsCdnUrlBase = cdn.defaultBase3 + "emojione/" + emojioneVersion;
-                } else {
-                    emojioneJsCdnUrlBase = cdn.defaultBase + "/" + emojioneVersion;
-                }
+        if(window.emojione) {
+            emojione = window.emojione;
 
-                $.ajax({
-                    url: emojioneJsCdnUrlBase + "/lib/js/emojione.min.js",
-                    dataType: "script",
-                    cache: true,
-                    success: function () {
-                        emojione = window.emojione;
-                        emojioneVersion = detectVersion(emojione);
-                        emojioneSupportMode = getSupportMode(emojioneVersion);
-                        var sprite;
-                        if (emojioneSupportMode > 4) {
-                            cdn.base = cdn.defaultBase3 + "emojione/assets/" + emojioneVersion;
-                            sprite = cdn.base + "/sprites/emojione-sprite-" + emojione.emojiSize + ".css";
-                        } else {
-                            cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
-                            sprite = cdn.base + "/sprites/emojione.sprites.css";
-                        }
-                        if (options.sprite) {
-                            if (document.createStyleSheet) {
-                                document.createStyleSheet(sprite);
-                            } else {
-                                $('<link/>', {rel: 'stylesheet', href: sprite}).appendTo('head');
-                            }
-                        }
-                        while (readyCallbacks.length) {
-                            readyCallbacks.shift().call();
-                        }
-                        cdn.isLoading = false;
-                    }
-                });
+            emojioneVersion = detectVersion(emojione);
+
+            emojioneSupportMode = getSupportMode(emojioneVersion);
+
+            var sprite;
+            if (emojioneSupportMode > 4) {
+                cdn.base = cdn.defaultBase3 + "emojione/assets/" + emojioneVersion;
+                sprite = cdn.base + "/sprites/emojione-sprite-" + emojione.emojiSize + ".css";
             } else {
-                emojioneVersion = detectVersion(emojione);
-                emojioneSupportMode = getSupportMode(emojioneVersion);
-                if (emojioneSupportMode > 4) {
-                    cdn.base = cdn.defaultBase3 + "emojione/assets/" + emojioneVersion;
+                cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
+                sprite = cdn.base + "/sprites/emojione.sprites.css";
+            }
+            if (options.sprite) {
+                if (document.createStyleSheet) {
+                    document.createStyleSheet(sprite);
                 } else {
-                    cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
+                    $('<link/>', {rel: 'stylesheet', href: sprite}).appendTo('head');
+                }
+            }
+            while (readyCallbacks.length) {
+                readyCallbacks.shift().call();
+            }
+        } else {
+            if (!cdn.isLoading) {
+                if (!emojione || getSupportMode(detectVersion(emojione)) < 2) {
+                    cdn.isLoading = true;
+                    let emojioneJsCdnUrlBase;
+                    if (getSupportMode(emojioneVersion) > 5) {
+                        emojioneJsCdnUrlBase = cdn.defaultBase3 + "npm/emojione@" + emojioneVersion;
+                    } else if (getSupportMode(emojioneVersion) > 4) {
+                        emojioneJsCdnUrlBase = cdn.defaultBase3 + "emojione/" + emojioneVersion;
+                    } else {
+                        emojioneJsCdnUrlBase = cdn.defaultBase + "/" + emojioneVersion;
+                    }
+
+                    $.ajax({
+                        url: emojioneJsCdnUrlBase + "/lib/js/emojione.min.js",
+                        dataType: "script",
+                        cache: true,
+                        local: true,
+                        success: function () {
+                            emojione = window.emojione;
+                            emojioneVersion = detectVersion(emojione);
+                            emojioneSupportMode = getSupportMode(emojioneVersion);
+
+                            var sprite;
+                            if (emojioneSupportMode > 4) {
+                                cdn.base = cdn.defaultBase3 + "emojione/assets/" + emojioneVersion;
+                                sprite = cdn.base + "/sprites/emojione-sprite-" + emojione.emojiSize + ".css";
+                            } else {
+                                cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
+                                sprite = cdn.base + "/sprites/emojione.sprites.css";
+                            }
+                            if (options.sprite) {
+                                if (document.createStyleSheet) {
+                                    document.createStyleSheet(sprite);
+                                } else {
+                                    $('<link/>', {rel: 'stylesheet', href: sprite}).appendTo('head');
+                                }
+                            }
+                            while (readyCallbacks.length) {
+                                readyCallbacks.shift().call();
+                            }
+                            cdn.isLoading = false;
+                        }
+                    });
+                } else {
+                    emojioneVersion = detectVersion(emojione);
+
+                    emojioneSupportMode = getSupportMode(emojioneVersion);
+
+                    if (emojioneSupportMode > 4) {
+                        cdn.base = cdn.defaultBase3 + "emojione/assets/" + emojioneVersion;
+                    } else {
+                        cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
+                    }
                 }
             }
         }
 
         emojioneReady(function() {
-            var emojiSize = "";
+            let emojiSize = "";
             if (options.useInternalCDN) {
                 if (emojioneSupportMode > 4) emojiSize = emojione.emojiSize + "/";
 
